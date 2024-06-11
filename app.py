53ee3fedc6403e9e42c132f17816cf57a34230cb
Brieuc de Ghellinck <brieucdg@gmail.com>
2024-06-11 17:11:05 +0200

    output_improved

--- app.py
+++ app.py
@@ -47,41 +47,43 @@ def main():
     }
 
     # Initialize session state
+    introduction_line = "Hi! How are you feeling today?"
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
+        st.session_state.chat_history = [{"role": "assistant", "content": introduction_line}]
         st.session_state.exchange_count = 0
+        st.session_state.prediction = 0
+        st.session_state.open = True
 
     # Display conversation
     for message in st.session_state.chat_history:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    # First line of conversation
-    if st.session_state.exchange_count == 0:
-        introduction_line = "Hi! How are you feeling today?"
-        st.chat_message("assistant").markdown(introduction_line)
+    # Keep chatbot open until conditions are met
+    if st.session_state.open:
 
-    # Text input for manual typing
-    user_input = st.chat_input("Talk to your AI Buddy...")
-    if user_input:
-        if st.session_state.exchange_count < 4:
+        # Text input for manual typing
+        user_input = st.chat_input("Talk to your AI Buddy...")
+        if user_input:
             st.chat_message("user").markdown(user_input)
-            st.session_state.chat_history.append({"role":"user","content":user_input})
+            st.session_state.chat_history.append({"role": "user", "content": user_input})
             st.session_state.exchange_count += 1
-
             messages = [
-                    {"role": "system", "content": '''You're a chatbot that's supposed to help people who are depressed or even suicidal.
-    The aim is to ask them simple questions to try understand their feelings.
-    Try to act as a psychologist.
-    Be concise'''},
-                    *st.session_state.chat_history
-                    ]
-
+                        {"role": "system", "content": '''You're a friendly and caring chatbot
+                     that's been trained to help people who are feeling depressed or suicidal.
+                     Your goal is to provide a safe and supportive space for users to express their feelings and thoughts.
+                     You should ask open-ended questions to encourage users to talk, and actively listen to their responses.
+                     If the user's responses indicate a high level of suicidal risk,
+                     you should provide them with resources and support to help them stay safe.
+                     However, if the user's responses indicate a lower level of risk,
+                     you can engage in a more casual conversation while still being mindful of their emotional state.'''},
+                        *st.session_state.chat_history
+                        ]
             data = {
-            "model": "gpt-3.5-turbo",
-            "messages": messages,
-            "max_tokens": 75
-        }
+                "model": "gpt-3.5-turbo",
+                "messages": messages,
+                "max_tokens": 75
+            }
             response = requests.post(url, headers=headers, data=json.dumps(data))
             response_data = json.loads(response.text)
             assistant_response = response_data["choices"][0]["message"]["content"]
@@ -91,16 +93,68 @@ def main():
             # Display assistant answer
             with st.chat_message("assistant"):
                 st.markdown(assistant_response)
-        else :
-            st.chat_message("user").markdown(user_input)
-            st.session_state.chat_history.append({"role":"user","content":user_input})
-            st.warning("The conversation has ended. Analyzing the conversation...")
 
             # Getting user content for prediction
             user_messages = "".join(item["content"] for item in st.session_state.chat_history if item["role"] == 'user')
-            prediction = f"Prediction: {get_prediction(user_messages)}"
-            st.session_state.chat_history.append({"role": "model", "content": prediction})
-            st.chat_message("model").markdown(prediction)
+            st.session_state.prediction = get_prediction(user_messages)
+
+            # If too many session or prediction to high the chat closes
+            if st.session_state.prediction > 0.85:
+                st.warning("End of conversation")
+                end_prompt = '''You're a friendly and caring chatbot operating in Belgium
+                     and you've been trained to help people who are feeling depressed or suicidal.
+                     Your goal is to provide a safe and supportive space for users to express their feelings and thoughts.
+                     The user you currently talking with has showed great signs of distress.
+                     Can you give him some advice and point him in the right direction?
+                     Be caring while giving some good advice.
+                     However, the situation is really concerning'''
+                end_message = [
+                    {"role": "system", "content": end_prompt},
+                    *st.session_state.chat_history
+                    ]
+                data = {
+                    "model": "gpt-3.5-turbo",
+                    "messages": end_message,
+                    "max_tokens": 1000
+                }
+                response = requests.post(url, headers=headers, data=json.dumps(data))
+                response_data = json.loads(response.text)
+                end_assistant_response = response_data["choices"][0]["message"]["content"]
+                with st.chat_message("assistant"):
+                    st.markdown(end_assistant_response)
+                    st.markdown("In Belgium, you can call 1813, the suicide prevention hotline. They're available 24/7 to listen and help.")
+                st.session_state.open = False
+
+            elif st.session_state.prediction > 0.5 and st.session_state.exchange_count > 5:
+                st.warning("End of conversation")
+                end_prompt = '''You are a friendly, caring chatbot operating in Belgium and have been trained to help people who are feeling depressed or suicidal.
+                Your goal is to provide users with a safe and supportive space to express their feelings and thoughts.
+                The user you're talking to is showing signs of sadness. However, the situation doesn't seem to be really worrying.
+                Can you give him some advice to cheer him up? Be caring while giving some good advice.'''
+                end_message = [
+                    {"role": "system", "content": end_prompt},
+                    *st.session_state.chat_history
+                    ]
+                data = {
+                    "model": "gpt-3.5-turbo",
+                    "messages": end_message,
+                    "max_tokens": 1000
+                }
+                response = requests.post(url, headers=headers, data=json.dumps(data))
+                response_data = json.loads(response.text)
+                end_assistant_response = response_data["choices"][0]["message"]["content"]
+                with st.chat_message("assistant"):
+                    st.markdown(end_assistant_response)
+                st.session_state.open = False
+
+            elif st.session_state.exchange_count > 5:
+                st.warning("End of conversation")
+                with st.chat_message("assistant"):
+                    st.markdown("Thank you for this nice discussions! Have a nice day")
+                st.session_state.open = False
+
+            else:
+                st.session_state.open = True
 
 if __name__ == "__main__":
     main()
