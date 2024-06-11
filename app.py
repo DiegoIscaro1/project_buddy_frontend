d0f1702ac2ff4ea631a0646332543f3d1727163c
Brieuc de Ghellinck <brieucdg@gmail.com>
2024-06-11 14:55:17 +0200

    chatbot integrated to front-end

--- app.py
+++ app.py
@@ -1,38 +1,19 @@
 import streamlit as st
+import openai
 import requests
-import os
+import json
 
-'''def recognize_speech():
-    recognizer = sr.Recognizer()
-    with sr.Microphone() as source:
-        st.write("Listening...")
-        audio = recognizer.listen(source)
-        st.write("Recognizing...")
-        try:
-            text = recognizer.recognize_google(audio)
-            st.write(f"You said: {text}")
-            return text
-        except sr.UnknownValueError:
-            st.write("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            st.write(f"Could not request results from Google Speech Recognition service; {e}")
-    return None'''
-
-def get_prediction(user_input):
+def get_prediction(user_input: str) -> int:
     url = "https://buddyapi-qncgwxayla-ew.a.run.app/predict"
     params = {"txt": user_input}
     response = requests.get(url, params=params)
     if response.status_code == 200 and "prediction" in response.json():
         return response.json()["prediction"]
     else:
-        return "An error occurred while fetching the prediction."
-
-def chatbot(user_input):
-    output = get_prediction(user_input)
-    return output
+        return f"An Error {response.status_code} has occurred while retrieving the prediction."
 
 def main():
-    st.set_page_config(page_title="Chatbot", page_icon="🤖")
+    st.set_page_config(page_title="Chatbot", page_icon="🤖", layout="centered")
 
     # Custom styling for the title
     st.markdown(
@@ -52,33 +33,74 @@ def main():
         unsafe_allow_html=True
     )
 
-    st.markdown('<p class="title">Buddy</p>', unsafe_allow_html=True)
+    st.title('🤖 Your AI Buddy')
 
-    # Sidebar image
-    image_url = "https://github.com/KiruaaSan/project_buddy_frontend/blob/master/Buddy.jpg"  # Replace this with your image URL
-    st.sidebar.image(image_url, width=100)
+    # Get OpenAI key
+    key = st.secrets["openai"]["OpenAI_key"]
+    openai.api_key = key
 
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    # Set up the API endpoint and headers
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {key}"  # Replace 'key' with your actual API key
+    }
 
-    def submit():
-        user_input = st.session_state.user_input
-        if user_input:
-            with st.spinner("The chatbot is thinking..."):
-                response = chatbot(user_input)
-            st.session_state.messages.append(("You", user_input))
-            st.session_state.messages.append(("Chatbot", response))
-            st.session_state.user_input = ""
+    # Initialize session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+        st.session_state.exchange_count = 0
+
+    # Display conversation
+    for message in st.session_state.chat_history:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # First line of conversation
+    if st.session_state.exchange_count == 0:
+        introduction_line = "Hi! How are you feeling today?"
+        st.chat_message("assistant").markdown(introduction_line)
 
     # Text input for manual typing
-    st.text_input("Type your message here...", key="user_input", on_change=submit)
+    user_input = st.chat_input("Talk to your AI Buddy...")
+    if user_input:
+        if st.session_state.exchange_count < 4:
+            st.chat_message("user").markdown(user_input)
+            st.session_state.chat_history.append({"role":"user","content":user_input})
+            st.session_state.exchange_count += 1
 
-    if st.session_state.messages:
-        for sender, message in st.session_state.messages:
-            if sender == "You":
-                st.markdown(f'<div style="text-align: right; margin-bottom: 10px;"><div style="display: inline-block; background-color: #d3e0ff; padding: 10px; border-radius: 10px;">{message}</div></div>', unsafe_allow_html=True)
-            else:
-                st.markdown(f'<div style="text-align: left; margin-bottom: 10px;"><div style="display: inline-block; background-color: #D3D3D3; padding: 10px; border-radius: 10px;">{message}</div></div>', unsafe_allow_html=True)
+            messages = [
+                    {"role": "system", "content": '''You're a chatbot that's supposed to help people who are depressed or even suicidal.
+    The aim is to ask them simple questions to try understand their feelings.
+    Try to act as a psychologist.
+    Be concise'''},
+                    *st.session_state.chat_history
+                    ]
+
+            data = {
+            "model": "gpt-3.5-turbo",
+            "messages": messages,
+            "max_tokens": 75
+        }
+            response = requests.post(url, headers=headers, data=json.dumps(data))
+            response_data = json.loads(response.text)
+            assistant_response = response_data["choices"][0]["message"]["content"]
+
+            st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+            # Display assistant answer
+            with st.chat_message("assistant"):
+                st.markdown(assistant_response)
+        else :
+            st.chat_message("user").markdown(user_input)
+            st.session_state.chat_history.append({"role":"user","content":user_input})
+            st.warning("The conversation has ended. Analyzing the conversation...")
+
+            # Getting user content for prediction
+            user_messages = "".join(item["content"] for item in st.session_state.chat_history if item["role"] == 'user')
+            prediction = f"Prediction: {get_prediction(user_messages)}"
+            st.session_state.chat_history.append({"role": "model", "content": prediction})
+            st.chat_message("model").markdown(prediction)
 
 if __name__ == "__main__":
     main()
